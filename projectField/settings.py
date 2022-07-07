98ea9e85fad8e5a66047ef5ec89a459e84f377a1
Gabriel Magalhaes Do Nascimento <gmnascimento@stefanini.com>
2022-07-07 16:44:28 -0300

    Pastas front Criadas 2

--- projectField/settings.py
+++ projectField/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'projectField.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
