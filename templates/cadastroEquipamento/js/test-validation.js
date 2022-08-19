8b1b6a5f59d843a68291b4b2240f6e92c74575a0
wesleyasilva <wa-silva@outlook.com>
2022-08-19 16:58:45 -0300

    teste da tabela dinâmica Solicitação

--- templates/cadastroEquipamento/js/test-validation.js
+++ templates/cadastroEquipamento/js/test-validation.js
@@ -1,33 +1,15 @@
 (() => {
     'use strict'
 
-    const marca = document.querySelector('#marca')
-    const periferico = document.querySelector('#periferico')
+    let t = document.querySelector('#tblsolicitacoes')
 
-    marca.innerHTML = "<option value='none'></option>"
-
-    let headset = [
-        { value: "Felitron", text: "Felitron" },
-        { value: "TopUse", text: "TopUse" }
-    ]
-
-    let others = [
-        { value: "Dell", text: "Dell" },
-        { value: "Lenovo", text: "Lenovo" },
-        { value: "Genérico", text: "Genérico" }
-    ]
-
-    periferico.onchange = event => {
-        if (periferico.options[periferico.selectedIndex].value === 'Headset') {
-            addOption(marca)
-            headset.forEach(m => marca.add(new Option(m.text, m.value)))
-        } else {
-            addOption(marca)
-            others.forEach(o => marca.add(new Option(o.text, o.value)))
-        }
-    }
-
-    function addOption(element) {
-        element.innerHTML = "<option value='none'></option>"
-    }
+    
+    Array.from(t.rows).forEach((tr, row_ind) => {
+        //console.log(tr.cells)
+        Array.from(tr.cells).forEach((cell, col_ind) => {
+            //console.log(cell.textContent)
+            //console.log('Value at row/col [' + row_ind + ',' + col_ind + '] = ' + cell.textContent);
+        });
+    });
+    
 })()
