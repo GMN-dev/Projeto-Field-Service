05a973d57fa80c9e2862ef8ef5c987db7b94dc00
Gabriel M. N <g.magalhaes.dev@gmail.com>
2022-08-23 21:28:29 -0300

    API 1.1

--- cadastroEquipamento/api/serializers.py
+++ cadastroEquipamento/api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class SolicitacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Solicitacao
-        fields = ('id_incidentes', 'chamado', 'informante', 'operacao')
+        fields = ('id_incidentes', 'chamado', 'data_incidente', 'operacao', 'andar', 'informante', 'periferico', 'motivo_solicitacao')
