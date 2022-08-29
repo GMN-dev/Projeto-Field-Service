11627377c3788d49dd7a80f20f1c3169f2eaa93c
Wesley Adam Oliveira Silva <wasilva10@stefanini.com>
2022-08-29 17:36:15 -0300

    Alterações no db e atualização no front: consumido API para criação da tabela dinâmica

--- templates/cadastroEquipamento/js/test-validation.js
+++ templates/cadastroEquipamento/js/test-validation.js
@@ -1,14 +1,4 @@
 (() => {
     'use strict'
 
-    let t = document.querySelector('#tblsolicitacoes')
-    
-    Array.from(t.rows).forEach((tr, row_ind) => {
-        console.log(tr.cells)
-        Array.from(tr.cells).forEach((cell, col_ind) => {
-            //console.log(cell.textContent)
-            //console.log('Value at row/col [' + row_ind + ',' + col_ind + '] = ' + cell.textContent);
-        });
-    });
-    
 })()
