2deb617bca6a5489622225d69567427a54e16db3
Gabriel M <g.magalhaes.dev@gmail.com>
2022-08-29 23:16:19 -0300

    commitlixo

--- cadastroEquipamento/api/serializers.py
+++ cadastroEquipamento/api/serializers.py
@@ -6,3 +6,4 @@ class SolicitacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Solicitacao
         fields = ('id_incidentes', 'chamado', 'data_incidente', 'operacao', 'andar', 'informante', 'periferico', 'motivo_solicitacao')
+
